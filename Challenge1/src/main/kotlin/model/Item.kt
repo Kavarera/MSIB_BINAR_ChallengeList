b1b39d0950d15e5549ed4408aa73b7e643e9130f
Kavarera <rafli.030715@gmail.com>
2023-09-29 19:21:53 +0700

    innit branch

--- Challenge1/src/main/kotlin/model/Item.kt
+++
@@ -1,6 +0,0 @@
-package model
-
-data class Item(
-    val Nama:String,
-    val Harga:Int
-)
--- Challenge2/app/src/main/java/com/example/challenge2/models/RecyclerViewLayoutOption.kt
+++
@@ -1,5 +0,0 @@
-package com.example.challenge2.models
-
-enum class RecyclerViewLayoutOption {
-    GRID_LAYOUT, LINEAR_LAYOUT
-}
